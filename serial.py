6c4629b1e885e14957cd7760919fa70e92fa2222
C4ptnS4lty <jonlahmann@gmail.com>
2024-04-21 12:58:08 -0400

    completed

--- serial.py
+++ serial.py
@@ -20,3 +20,18 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start = 0):
+        """ Initialize generator. If start is not given, will set start at 0"""
+        self.start = start
+        self.count = 0
+
+    def generate(self):
+        """returns the next number in the serial"""
+        self.count += 1
+        return self.start + self.count - 1
+
+        
+    def reset(self):
+        """Reset SerialGenerator's count back to 0"""
+        self.count = 0
+
--- wordfinder.py
+++ wordfinder.py
@@ -2,4 +2,61 @@
 
 
 class WordFinder:
-    ...
+    """Initializes with a text document dictionary
+    
+    >>> wf = WordFinder("/Users/student/words.txt")
+    3 words read
+
+    >>> wf.random()
+    'cat' or 'porcupine' or 'dog'
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'porcupine'
+
+    >>> wf.random()
+    'dog'
+    """
+
+    def __init__(self, file) -> None:
+        self.list = self.create(file)
+
+        return f'{len(self.list)} words read'
+    
+    def create(file):
+        items = []
+
+        with open(file, 'r') as file:
+            for line in file:
+                items.append(line)
+
+        return items
+
+    def random(self):
+        """Returns a random instance from the list"""
+        import random
+        return (random.choice(self.list))
+    
+"""Child of wordFinder. 
+    Makes sure to avoid empty strings
+    or strings starting with #
+"""
+class SpecialWordFinder(WordFinder):
+    
+    def create(file):
+
+        items = []
+
+        with open(file, 'r') as file:
+            for line in file:
+                if((not len(line) == 0) or (not line[0] == '#')):
+                    items.append(line)
+
+        return items
+
+    
+
+        
+
